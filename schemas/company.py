1e3a292f095a9ee061cb7eed99d996f5e6677dcf
MOHAMMED NAJEEBULLA <mohammednajeebulla2004@gmail.com>
2026-06-27 14:09:08 +0530

    company_job

--- schemas/company.py
+++ schemas/company.py
@@ -1,10 +1,24 @@
 from pydantic import BaseModel
 from typing import Optional
+from .job import JobResponse
 
-class CompanyCreate(BaseModel):
-    name: str
-    location : str
+class CompanyBase(BaseModel):
+    name : str
+    email :str
+    phone : str
+
+class CompanyCreate(CompanyBase):
+    pass
     
-class CompanyUpdate(BaseModel):
+class CompanyUpdate(CompanyBase):
     name : Optional[str] = None
-    location : Optional[str] = None
+    email : Optional[str] = None
+    phone: Optional[str] = None
+    
+    
+class CompanyResponse(CompanyBase):
+    id:int
+    jobs: list[JobResponse]
+    
+    class Config:
+        from_attributes = True
